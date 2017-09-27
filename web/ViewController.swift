44a2536e53bd02c48da1ec966948fa5ab153e359
Ahmed Burham <ahmed_burham@Ahmeds-Mac.local>
2017-09-27 17:33:02 +0100

    Initial Commit

---
+++ web/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  web
+//
+//  Created by Ahmed Burham on 9/27/17.
+//  Copyright © 2017 Ahmed Burham. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
